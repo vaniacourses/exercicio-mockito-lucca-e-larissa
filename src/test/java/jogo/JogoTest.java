d745051382103839be5a41de7aaee7493f4fac55
Lucca Gomes <lucca.gomes@gabriel.com.br>
2025-09-15 19:11:13 -0300

    test: Inicializando a classe de teste

--- src/test/java/jogo/JogoTest.java
+++ src/test/java/jogo/JogoTest.java
@@ -1,6 +1,24 @@
 package jogo;
 
-public class JogoTest {
-	
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class JogoTest {
+
+    private Jogador jogador;
+    private Dado dado1;
+    private Dado dado2;
+    private Jogo jogo;
+
+    @BeforeEach
+    void inicializa() {
+        Jogador jogador = mock(Jogador.class);
+        Dado dado1   = mock(Dado.class);
+        Dado dado2   = mock(Dado.class);
+        Jogo jogo = new Jogo(jogador, dado1, dado2);
+    }
 
 }
